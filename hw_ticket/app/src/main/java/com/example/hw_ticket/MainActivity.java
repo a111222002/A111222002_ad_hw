5ce2b131c7aa3d2197bd5134f02ef87df6859c14
a111222002 <a111222002@mail.shu.edu.tw>
2024-04-26 13:58:02 +0800

    2024-4-26

--- hw_ticket/app/src/main/java/com/example/hw_ticket/MainActivity.java
+++ hw_ticket/app/src/main/java/com/example/hw_ticket/MainActivity.java
@@ -1,3 +1,4 @@
+//A111222002
 package com.example.hw_ticket;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,76 +13,93 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements RadioGroup.OnCheckedChangeListener, TextWatcher {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, TextWatcher {
     private TextView output;
     private EditText txt;
     private RadioGroup rgender;
     private RadioGroup rticket;
 
     private Button submit;
-    private  String str = "0";
-    private  String gender="";
-    private String[] ticketType={"",""};
-    private  String ticket="";
-    private  int sum=0;
+    private String str = "0";
+    private String gender = "";
+    private String[] ticketType = {"", ""};
+    private String ticket = "";
+    private int sum = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        output = (TextView) findViewById(R.id.lblOutput);
-        rgender = (RadioGroup) findViewById(R.id.rgGender);
-        rticket = (RadioGroup) findViewById(R.id.rgticket);
+        output = findViewById(R.id.lblOutput);
+        rgender = findViewById(R.id.rgGender);
+        rticket = findViewById(R.id.rgticket);
         rgender.setOnCheckedChangeListener(this);
         rticket.setOnCheckedChangeListener(this);
-        txt = (EditText) findViewById(R.id.txtName);
+        txt = findViewById(R.id.txtName);
         txt.addTextChangedListener(this);
-
+        txt.setInputType(android.text.InputType.TYPE_CLASS_NUMBER); // Set input type to number
+        rticket.setEnabled(false); // Disable ticket selection initially
     }
+
     @Override
     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
     }
     @Override
+    public void afterTextChanged(Editable editable) {
+    }
+
+    @Override
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
         calculateSum();
     }
-    @Override
-    public void afterTextChanged(Editable editable) {
-    }
+
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int i) {
         if (radioGroup == rgender) {
-            show(i, "rgender");
+            gender = ((RadioButton) findViewById(i)).getText().toString();
+            rticket.setEnabled(true);
         } else {
             show(i, "rticket");
         }
         calculateSum();
     }
+
     private void calculateSum() {
         if (!txt.getText().toString().isEmpty()) {
             int ticketCount = Integer.parseInt(txt.getText().toString());
-            int ticketPrice = Integer.parseInt(ticketType[1]);
-            str = ticketCount + "張";
-            sum = ticketCount * ticketPrice;
+            if (!gender.isEmpty() && !ticket.isEmpty()) {
+                int ticketPrice = Integer.parseInt(ticketType[1]);
+                str = ticketCount + "張";
+                sum = ticketCount * ticketPrice;
+            } else {
+                str = "0張";
+                sum = 0;
+            }
         } else {
             str = "0張";
             sum = 0;
         }
-        output.setText("性別："+gender + "\n" +"票別(數量)："+ ticketType[0] + str + "\n" + "金額：" + sum);
+        output.setText("性別：" + gender + "\n" + "票別,數量：" + ticketType[0] +","+ str + "\n" + "金額：" + sum);
     }
+
     public void show(int i, String g) {
         RadioButton selected = findViewById(i);
-        if (g.equals("rgender")) {
-            gender = selected.getText().toString();
-        } else if (g.equals("rticket")) {
+        if (g.equals("rticket")) {
             ticket = selected.getText().toString();
             ticketType = ticket.split("\n");
         }
     }
-    public void submit(View view){
-        Intent intent=new Intent(this, result.class);
-        intent.putExtra("result",output.getText());
-        startActivity(intent);
+
+    public void submit(View view) {
+        if (sum > 0) {
+            Intent intent = new Intent(this, result.class);
+            intent.putExtra("result", output.getText());
+            startActivity(intent);
+        } else {
+
+            Toast.makeText(this, "至少購買一張票", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- hw_ticket/app/src/main/java/com/example/hw_ticket/result.java
+++ hw_ticket/app/src/main/java/com/example/hw_ticket/result.java
@@ -3,6 +3,7 @@ package com.example.hw_ticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class result extends AppCompatActivity {
@@ -11,7 +12,7 @@ public class result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        TextView result=(TextView) findViewById(R.id.txtresult);
+        TextView result = findViewById(R.id.txtresult);
         result.setText(getIntent().getStringExtra("result"));
     }
 }
